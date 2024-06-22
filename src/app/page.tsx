8ec8496c9faa112a4008d62b2a5a308a7f31d317
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-06-22 14:05:12 +0530

    Instructors section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import Hero from "@/components/Hero";
+import Instructors from "@/components/Instructors";
 import Testimonials from "@/components/Testimonials";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -13,6 +14,7 @@ export default function Home() {
         <WhyChooseUs />
         <Testimonials />
         <UpcomingWebinars />
+        <Instructors />
       </main>
     </>
   );

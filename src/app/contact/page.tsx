ee742f0dd8557096547963ebe92ccbab15bfc98d
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-06-22 14:56:57 +0530

    Contact Added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -47,15 +47,21 @@ function MusicSchoolContactUs() {
               />
             </LabelInputContainer>
             <LabelInputContainer className="mb-4">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" placeholder="••••••••" type="password" />
-            </LabelInputContainer>
-            <LabelInputContainer className="mb-8">
-              <Label htmlFor="twitterpassword">Your twitter password</Label>
-              <Input
-                id="twitterpassword"
-                placeholder="••••••••"
-                type="twitterpassword"
+              <Label htmlFor="message">Message</Label>
+              <textarea
+                id="message"
+                className={cn(
+                  `flex h-20 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent"
+                  file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600
+                  focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+                  disabled:cursor-not-allowed disabled:opacity-50
+                  dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+                  group-hover/input:shadow-none transition duration-400`,
+                  "w-full"
+                )}
+                placeholder="Your message here..."
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
               />
             </LabelInputContainer>
 
